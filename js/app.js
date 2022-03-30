5e8a681630dac9e18de910eb3e19cd12558b55be
Omar Mohamed <omar.mohamed4799@gmail.com>
2022-03-30 21:08:59 +0300

    Updates

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const loader = document.querySelector('.loader');
+const form = document.querySelector('.form');
+
+window.addEventListener('load', () => {
+    loader.classList.add('hide');
+    form.classList.add('animate');
+})
